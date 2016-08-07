dfbfd592b8b15907c9f7596bf400f75d5312366e
Sykes8 <j.slivovsky@hotmail.com>
2016-08-07 11:58:07 +0200

    Hmmm

--- src/main/java/sykes8/EnderCrystals/proxy/ClientProxy.java
+++ src/main/java/sykes8/EnderCrystals/proxy/ClientProxy.java
@@ -1,12 +1,9 @@
 package sykes8.EnderCrystals.proxy;
 
-<<<<<<< HEAD
 import sykes8.EnderCrystals.ModBlocks;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.client.model.ModelLoader;
-=======
->>>>>>> parent of 217bc68... Stupid Textures
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -18,7 +15,7 @@ public class ClientProxy extends CommonProxy
     public void preInit(FMLPreInitializationEvent e) 
     {
         super.preInit(e);
-       // ModRenderers.preInit();
+        ModBlocks.initModels();
     }
 
     @Override
